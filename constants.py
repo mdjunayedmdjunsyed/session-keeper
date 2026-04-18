3e7d9b9c422c5fc29b22512fc6673143c8d54ea0
0xwalker <mdjunayedmdjunsyed@gmail.com>
2026-04-18 09:00:23 +0100

    cleanup and reorganization of constants

---
+++ constants.py
@@ -0,0 +1,24 @@
+FPS_LIMIT = 60
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+PLAYER_SPEED = 16
+JUMP_HEIGHT = 10
+COLORS = {
+    'background': (0, 0, 0),
+    'player': (255, 255, 255),
+    'obstacle': (255, 0, 0),
+}
+LEVELS = {
+    'easy': {'time_limit': 300, 'enemy_count': 5},
+    'medium': {'time_limit': 200, 'enemy_count': 10},
+    'hard': {'time_limit': 100, 'enemy_count': 15},
+}
+MAX_PLAYERS = 10
+GRAVITY = 9.8
+WEAPON_TYPES = ['sword', 'bow', 'magic']
+CHARACTER_CLASSES = ['warrior', 'archer', 'mage']
+POWER_UPS = {
+    'speed_boost': 5,
+    'damage_increase': 2,
+    'health_regen': 1,
+}
