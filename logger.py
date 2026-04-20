bebe20b060823d38aa48764b43247f6d46564c5e
0xwalker <mdjunayedmdjunsyed@gmail.com>
2026-04-20 09:01:00 +0100

    setup logger with rotation

--- logger.py
+++ logger.py
@@ -1,30 +1,21 @@
 import logging
+import os
 from logging.handlers import RotatingFileHandler
 
-class Logger:
-    def __init__(self, name, log_file, max_bytes=5*1024*1024, backup_count=3):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+LOG_FILE = 'session_keeper.log'
+MAX_BYTES = 5 * 1024 * 1024  # 5 MB
+BACKUP_COUNT = 3
+
+
+def setup_logger():
+    logger = logging.getLogger('session_keeper')
+    logger.setLevel(logging.DEBUG)
+    if not logger.handlers:
+        handler = RotatingFileHandler(LOG_FILE, maxBytes=MAX_BYTES, backupCount=BACKUP_COUNT)
+        formatter = logging.Formatter(LOG_FORMAT)
         handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        logger.addHandler(handler)
+    return logger
 
-    def debug(self, msg):
-        self.logger.debug(msg)
-
-    def info(self, msg):
-        self.logger.info(msg)
-
-    def warning(self, msg):
-        self.logger.warning(msg)
-
-    def error(self, msg):
-        self.logger.error(msg)
-
-    def critical(self, msg):
-        self.logger.critical(msg)
-
-if __name__ == '__main__':
-    log = Logger('session_keeper', 'session_keeper.log')
-    log.info('Logger setup complete.')
+logger = setup_logger()
