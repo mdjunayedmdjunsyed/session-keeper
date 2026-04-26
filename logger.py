d7c4f1258d8a2a96a1717edb8b3e81ec5fd36e9c
0xwalker <mdjunayedmdjunsyed@gmail.com>
2026-04-26 09:00:19 +0100

    add set up rotating logger for sessions

--- logger.py
+++ logger.py
@@ -1,29 +1,17 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-class LoggerSetup:
-    def __init__(self, log_file='app.log', max_bytes=5*1024*1024, backup_count=3):
-        self.logger = logging.getLogger('SessionKeeper')
-        self.logger.setLevel(logging.DEBUG)
-        self.handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
-        self.handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-        self.logger.addHandler(self.handler)
+def setup_logger(log_file='session_keeper.log', max_size=5 * 1024 * 1024, backup_count=3):
+    logger = logging.getLogger('session_keeper')
+    logger.setLevel(logging.INFO)
+    handler = RotatingFileHandler(log_file, maxBytes=max_size, backupCount=backup_count)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.info('Logger setup complete')
+    return logger
 
-    def log_info(self, message):
-        self.logger.info(message)
-
-    def log_warning(self, message):
-        self.logger.warning(message)
-
-    def log_error(self, message):
-        self.logger.error(message)
-
-    def log_debug(self, message):
-        self.logger.debug(message)
-
-if __name__ == '__main__':
-    logger = LoggerSetup()  
-    logger.log_info('Logger initialized')
-    logger.log_warning('This is a warning message')
-    logger.log_error('An error occurred')
-    logger.log_debug('Debug information here')
+# Example usage: 
+# if __name__ == '__main__':
+#     log = setup_logger()
+#     log.info('This is an example log message.')
