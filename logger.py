d833581551a729f6f882162d77a64dd1548d55bf
Junayd Rahman <mdjunayedmdjunsyed@gmail.com>
2026-04-30 09:00:28 +0100

    fix implement logging utility for roblox

--- logger.py
+++ logger.py
@@ -1,31 +1,34 @@
 import logging
 
-class CustomLogger:
+class RobloxLogger:
     def __init__(self, name):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
-        handler = logging.StreamHandler()
-        handler.setLevel(logging.DEBUG)
+        handler = logging.FileHandler(f'{name}.log')
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
-        
+
     def debug(self, message):
         self.logger.debug(message)
-        
+
     def info(self, message):
         self.logger.info(message)
-        
+
     def warning(self, message):
         self.logger.warning(message)
-        
+
     def error(self, message):
         self.logger.error(message)
-        
+
     def critical(self, message):
         self.logger.critical(message)
-        
-    def performance_log(self, message):
-        self.logger.info(f'PERFORMANCE: {message}')  
 
-custom_logger = CustomLogger('SessionKeeperLogger')
+# Example usage
+if __name__ == '__main__':
+    logger = RobloxLogger('session_keeper')
+    logger.info('Roblox session started')
+    logger.debug('Debug information about session')
+    logger.warning('Potential issue detected')
+    logger.error('An error occurred')
+    logger.critical('Critical issue!')
